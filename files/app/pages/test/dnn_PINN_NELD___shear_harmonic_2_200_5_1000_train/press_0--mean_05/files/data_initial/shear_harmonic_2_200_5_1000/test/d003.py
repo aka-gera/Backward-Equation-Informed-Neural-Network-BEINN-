b758b7d5556099c7c553ba4af901e28bd3d9d8c5
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-08-05 00:18:53 -0400

    Add files via upload

---
+++ files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train/press_0--mean_05/files/data_initial/shear_harmonic_2_200_5_1000/test/d003.py
@@ -0,0 +1,57 @@
+ 
+
+import os, sys ,dash 
+file_path_org=os.getcwd()
+sys.path.append(file_path_org ) 
+file_path_org=os.path.join(os.path.dirname(file_path_org) ,'files')
+from  neld_fun_0.app_param_test import app_param
+from dash import callback  
+
+neld_names = ['d003']
+neld_namess = ['d003_', 'd003', 'd003']
+neld_path_inits =['shear_harmonic_2_200_5_1000_test']
+data_studied = 'test'  
+model_sufix = 'press_0--mean_05' 
+path_train= None
+path_file= None
+path_file_sub=None
+path_file_dir='/Users/akag/Desktop/Backward-Equation-Informed-Neural-Network-BEINN--main/files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train/press_0--mean_05/files/data_initial/shear_harmonic_2_200_5_1000/test/path_files.pkl'
+pinn_dir_data='None'
+neld_data=None
+index=3
+model_type='dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train'
+obj_org_path_dict=None
+model_sufix_dic=None
+path_display=None
+path_display_dic=None
+mapp = app_param(
+    file_path_org=file_path_org,
+    model_sufix=model_sufix,
+    path_train=path_train, 
+    path_file=path_file,
+    path_file_sub=path_file_sub,
+    path_file_dir=path_file_dir,
+    pinn_dir_data=pinn_dir_data,
+    index=index, 
+    data_studied=data_studied,  
+    neld_data=neld_data,
+    model_type=model_type,
+    obj_org_path_dict=obj_org_path_dict,
+    model_sufix_dic=model_sufix_dic,
+    path_display=path_display,
+    path_display_dic=path_display_dic,
+)
+
+title_neld_name = f'd003'
+dash.register_page(__name__, title=title_neld_name, name=title_neld_name, order=3) 
+
+def layout():
+    return mapp.app_layout
+
+@callback(
+    mapp.Output,
+    mapp.Input, 
+    prevent_initial_call=mapp.prevent_initial_call
+)
+def update_output(*args):  
+    return mapp.Get_output(*args)
---
+++ files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train_data.py
@@ -0,0 +1,18 @@
+
+
+import os, sys ,dash 
+sys.path.append(os.getcwd() ) 
+from neld_fun_0.side_bar import sidebar ,get_dnn,dnn_page 
+
+
+page_dir= '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train-data-' 
+page_name='dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train'  
+page_name_view=dnn_page()['results']
+forbidden_endswith=None 
+forbidden_endswith = None if forbidden_endswith in (None, 'None') else forbidden_endswith
+
+
+dash.register_page(__name__, title=page_name, name=page_name,order=0) 
+
+def layout():
+    return get_dnn(page_dir,page_name_view,forbidden_endswith,)
