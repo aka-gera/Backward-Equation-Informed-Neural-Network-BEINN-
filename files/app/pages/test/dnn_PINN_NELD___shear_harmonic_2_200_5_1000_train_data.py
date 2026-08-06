1872151d44e9d9335084218949e86c94732fdc16
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-08-06 04:23:45 -0400

    Update import path for sidebar and DNN functions

--- files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train_data.py
+++ files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train_data.py
@@ -2,7 +2,7 @@
 
 import os, sys ,dash 
 sys.path.append(os.getcwd() ) 
-from neld_fun_0.side_bar import sidebar ,get_dnn,dnn_page 
+from beinn.neld_fun_0.side_bar import sidebar ,get_dnn,dnn_page 
 
 
 page_dir= '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train-data-' 
