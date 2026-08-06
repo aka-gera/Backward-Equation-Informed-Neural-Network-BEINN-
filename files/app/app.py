2d041994b72481c2b9919d26344b456aa3f1fea2
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-08-06 02:10:23 -0400

    Update app.py

--- files/app/app.py
+++ files/app/app.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.getcwd()))
 import dash_bootstrap_components as dbc
 from dash import Dash, html
 import webbrowser, threading
-from neld_fun_0.side_bar import header_navbar
+from beinn.neld_fun_0.side_bar import header_navbar
 
 forbidden_page=('/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-test/press-0--mean-05/', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/press-0--mean-05/', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/posi--mean-05/', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/momen--mean-05/', '/dsa', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-test/press--mean-05/', '/dsa/test', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-test/momen--mean-05/', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-test/posi--mean-05/', '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/press--mean-05/')
 forbidden_endswith='None'
