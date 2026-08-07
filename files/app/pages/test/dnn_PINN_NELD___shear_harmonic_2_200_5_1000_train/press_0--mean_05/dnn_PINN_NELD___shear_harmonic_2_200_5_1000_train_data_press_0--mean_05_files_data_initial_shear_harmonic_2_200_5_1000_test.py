3b6ad90129dd8c6cb305cc10dc4dbe975570adca
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-08-06 23:27:10 -0400

    Add files via upload

---
+++ files/app/pages/test/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train/press_0--mean_05/dnn_PINN_NELD___shear_harmonic_2_200_5_1000_train_data_press_0--mean_05_files_data_initial_shear_harmonic_2_200_5_1000_test.py
@@ -0,0 +1,18 @@
+
+
+import os, sys ,dash 
+sys.path.append(os.getcwd() ) 
+from beinn.neld_fun_0.side_bar import sidebar ,get_dnn,dnn_page 
+
+
+page_dir= '/test/dnn-pinn-neld---shear-harmonic-2-200-5-1000-train/press-0--mean-05/files/data-initial/shear-harmonic-2-200-5-1000/test' 
+page_name='test'  
+page_name_view=dnn_page()['visualization']
+forbidden_endswith=None 
+forbidden_endswith = None if forbidden_endswith in (None, 'None') else forbidden_endswith
+
+
+dash.register_page(__name__, title=page_name, name=page_name,order=0) 
+
+def layout():
+    return get_dnn(page_dir,page_name_view,forbidden_endswith,)
