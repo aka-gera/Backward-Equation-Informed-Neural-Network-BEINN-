f052a4f1c6ca4eb2b06e20034a6307aa39c3b59f
Abdel Kader A Geraldo <37124404+aka-gera@users.noreply.github.com>
2026-08-07 20:26:20 -0400

    Normalize file path separators for consistency
    Replace backslashes with forward slashes in file paths.

--- beinn/neld_fun_0/app_get_pinn_neld.py
+++ beinn/neld_fun_0/app_get_pinn_neld.py
@@ -661,7 +661,7 @@ class class_data:
 
             ku,kuu='inference','true' 
             for mn,tyy,tyyy in zip(['initial','final'],self.intensity_logit_dict[ku],self.intensity_logit_dict[kuu]):
-                patth,patthy=self.path_file_sub[tyy][id_path],self.path_file_sub[tyyy][id_path]
+                patth,patthy=self.path_file_sub[tyy][id_path].replace('\\','/'),self.path_file_sub[tyyy][id_path].replace('\\','/')
 
                 print('[[[exist]]]',os.path.exists(patth), os.path.exists(patthy),patth,patthy)
                 if os.path.exists(patth) and os.path.exists(patthy): 
